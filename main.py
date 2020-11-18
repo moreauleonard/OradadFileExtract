632e421428b513a023100393a594c3cdf9e2851a
Léonard Moreau <leonard.moreau@epfedu.fr>
2020-11-19 00:34:44 +0100

    removed filepath

--- main.py
+++ main.py
@@ -3,14 +3,8 @@ from oradad_file import OradadFile
 from datetime import datetime
 from pathlib import Path
 
-PRIVATE_KEY = '/Users/leo/Documents/Programmation/test_oradad_uncrypt/ORADAD_Extract/private.pem'
-COMPRESSED_FILE = '//Users/leo/Documents/Programmation/test_oradad_uncrypt/compressed/metadata.tsv.oradad'
-ENCRYPTED_FILE = '/Users/leo/Documents/Programmation/test_oradad_uncrypt/encrypted/metadata.tsv.oradad'
-ENCRYPTED_AND_COMPRESSED_FILE = '/Users/leo/Documents/Programmation/test_oradad_uncrypt/ORADAD_tars/ace.lan_20201003-072800/ace.lan/20201003-072800/metadata.tsv.oradad'
-ENCRYPTED_AND_COMPRESSED_FILE = '/Users/leo/Documents/Programmation/test_oradad_uncrypt/ORADAD_tars/vsiparis.local_20201110-112110/vsiparis.local/20201110-112110/domaindns/vsiparis.local/top.tsv.oradad'
-
-
-ENC_DIR = "/Users/leo/Documents/Programmation/test_oradad_uncrypt/ORADAD_tars/ace.lan"
+PRIVATE_KEY = './private.pem'
+ENC_DIR = "./ORADAD_tars/domain.local"
 
 if __name__ == "__main__":
     start_time = datetime.now()
